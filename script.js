b690d2a07aceccc6accf4395d3826d5ae4e00499
Sugirtha-Angel <sugirthaangel@gmail.com>
2024-07-02 18:55:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+ function toggleMenu() {
+    const menu = document.querySelector(".phone-links");
+    const icon = document.querySelector(".phone-icons");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
